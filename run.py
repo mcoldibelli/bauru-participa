18804d056a7cc072bdc15c71fe930b0790144f0f
Marcelo Augusto Coldibelli <marcelo.coldibelli@gmail.com>
2025-03-29 16:09:11 -0300

    feat: finishes db connection with app

---
+++ run.py
@@ -0,0 +1,19 @@
+from database.database import create_app, db
+from database.models import User, Poll, PollOption, UserVote
+
+
+app = create_app()
+app.name = 'bauru_participa'
+
+def init_db():
+    try:
+        with app.app_context():
+            db.create_all()
+            print("Database initialized with success")
+    except Exception as e:
+        print(f"Error initializing database: ${str(e)}")
+        raise
+
+if __name__ == "__main__":
+    init_db()
+    app.run()
